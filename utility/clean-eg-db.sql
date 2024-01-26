65ff2b4e3072ff73cf706897a64a8310da44d044
Nate Vogel <nathan.c.vogel@gmail.com>
2024-01-26 15:00:54 -0500

    added query for cleaning db

---
+++ utility/clean-eg-db.sql
@@ -0,0 +1,8 @@
+
+
+update defendant set firstName = '', lastName = '', PP = 0, SID = '000-00-00-0', SSN='000-00-0000',DOB='2000-01-01',street='',street2='';
+
+update arrest set OTN='',DC=0,docketNumPrimary='',docketNumRelated='',arrestDate='2000-01-01',dispositionDate='2000-01-01',costsTotal=0,costsPaid=0,costsCharged=0,costsAdjusted=0,bailTotal=0,bailCharged=0,bailPaid=0,bailAdjusted=0,bailTotalToal=0,bailChargedTotal=0,bailPaidTotal=0,bailAdjustedTotal=0;
+
+
+
